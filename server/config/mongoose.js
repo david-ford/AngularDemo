2e4adbebf8aedaea2975f4ceddcc3cfd73d5dea0
david-ford <poster55@gmail.com>
2015-05-27 14:27:07 -0500

    Added password hashing.

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,36 +1,63 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    crypto = require('crypto');
 
-module.exports = function(config) {
+module.exports = function (config) {
     mongoose.connect(config.db);
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'error connecting to the database...'));
-    db.once('open', function callback(){
+    db.once('open', function callback() {
         console.log('angular db opened');
     });
 
     var userSchema = mongoose.Schema({
         firstName: String,
         lastName: String,
-        username: String
+        username: String,
+        salt: String,
+        hashed_pwd: String
     })
     var User = mongoose.model('User', userSchema);
-    User.find({}).exec(function(err, collection){
-        if(collection.length === 0) {
+    User.find({}).exec(function (err, collection) {
+        if (collection.length === 0) {
+            var salt, hash;
+            salt = createSalt();
+            hash = hashPwd(salt, 'tim');
             User.create({
-                firstName:'Tim',
+                firstName: 'Tim',
                 lastName: 'Rayburn',
-                username: 'trayburn'
+                username: 'trayburn',
+                salt: salt,
+                hash: hash
             });
+            var salt, hash;
+            salt = createSalt();
+            hash = hashPwd(salt, 'david');
             User.create({
-                firstName:'David',
+                firstName: 'David',
                 lastName: 'Ford',
-                username: 'dford'
+                username: 'dford',
+                salt: salt,
+                hash: hash
             });
+            var salt, hash;
+            salt = createSalt();
+            hash = hashPwd(salt, 'kevin');
             User.create({
-                firstName:'Kevin',
+                firstName: 'Kevin',
                 lastName: 'Berry',
-                username: 'kberry'
+                username: 'kberry',
+                salt: salt,
+                hash: hash
             });
         }
     })
+};
+
+function createSalt() {
+    return crypto.randomBytes(128).toString('base64');
+}
+
+function hashPwd(salt, pwd) {
+    var hmac = crypto.createHmac('sha1', salt);
+    return hmac.update(pwd).digest('hex');
 }
