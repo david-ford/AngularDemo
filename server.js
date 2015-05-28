44a3fdfbd17926eb9594c9950d17eb5495169632
david-ford <poster55@gmail.com>
2015-05-28 11:06:31 -0500

    Reimplemented IIFEs and fixed a bug with user login.

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-var express = require('express');
+(function () {
+    var express = require('express');
 
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+    var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var app = express();
+    var app = express();
 
-var config = require('./server/config/config')[env];
+    var config = require('./server/config/config')[env];
 
-require('./server/config/express')(app, config);
-require('./server/config/mongoose')(config);
-require('./server/config/routes')(app);
-require('./server/config/passport')();
+    require('./server/config/express')(app, config);
+    require('./server/config/mongoose')(config);
+    require('./server/config/routes')(app);
+    require('./server/config/passport')();
 
-app.listen(config.port);
-console.log('Listening on port ' + config.port + '...');
-console.log(env);
+    app.listen(config.port);
+    console.log('Listening on port ' + config.port + '...');
+    console.log(env);
+})();
--- server/config/config.js
+++ server/config/config.js
@@ -1,15 +1,17 @@
-var path = require('path');
-var rootPath = path.normalize(__dirname + '/../../')
+(function () {
+    var path = require('path');
+    var rootPath = path.normalize(__dirname + '/../../')
 
-module.exports = {
-    development: {
-        rootPath: rootPath,
-        db: 'mongodb://localhost/angulardb',
-        port: process.env.PORT || 3030
-    },
-    production: {
-        rootPath: rootPath,
-        db: 'mongodb://dford:angapppass@ds051160.mongolab.com:51160/angulardb',
-        port: process.env.PORT || 80
-    }
-};
+    module.exports = {
+        development: {
+            rootPath: rootPath,
+            db: 'mongodb://localhost/angulardb',
+            port: process.env.PORT || 3030
+        },
+        production: {
+            rootPath: rootPath,
+            db: 'mongodb://dford:angapppass@ds051160.mongolab.com:51160/angulardb',
+            port: process.env.PORT || 80
+        }
+    };
+})();
